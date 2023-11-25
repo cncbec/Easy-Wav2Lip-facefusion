9cb4c1b9ca3872ad653adc2a0962c893dd5b8b75
cncbec <54312958+cncbec@users.noreply.github.com>
2023-11-25 17:59:30 +0800

    Update inference_for_facefusion.py

--- inference_for_facefusion.py
+++ inference_for_facefusion.py
@@ -715,14 +715,24 @@ def main():
   
     if str(args.preview_settings) == 'False':
         print("converting to final video")
-
-        subprocess.check_call([
-            "ffmpeg", "-y", "-loglevel", "error",
-            "-i", "temp/result.mp4",
-            "-i", args.audio,
-            "-c:v", "h264_nvenc",
-            args.outfile,
-        ])
+	#查看音频文件路径下有没有srt字幕文件，如果有，则合并音频、视频、字幕。如果没有则只合并视频和音频
+        audio_path_part = os.path.dirname(args.audio)
+        srt_files = glob.glob(os.path.join(directory, '*.srt'))
+        font_path = '思源黑体旧字形 Bold'
+        if files:
+            ffmpeg_command = (
+            	f"ffmpeg -i 'temp/result.mp4' -i {args.audio} -vf subtitles={srt_files[0]}:force_style='FontName=\"{font_path}\",FontSize=20,PrimaryColour=16777215,Alignment=2,OutlineColour=0,Outline=1,Shadow=0,BorderStyle=1,MarginL=10,MarginR=10,MarginV=10,LineSpacing=10' "
+            	f"-c:v libx264 -c:a aac -b:a 192k -map 0:v -map 1:a {args.outfile}"
+            )
+            result = subprocess.check_call(ffmpeg_command, shell=True)
+        else:
+            subprocess.check_call([
+            	"ffmpeg", "-y", "-loglevel", "error",
+            	"-i", "temp/result.mp4",
+            	"-i", args.audio,
+            	"-c:v", "h264_nvenc",
+            	args.outfile,
+            ])
 
 
 model = detector = detector_model = None
